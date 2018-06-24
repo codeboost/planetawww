5e2552196eb4479db0a2b037915969df77d81059
Florin Braghis <florin@libertv.ro>
2018-06-24 19:37:36 +0200

    Home menu

--- src/cljs/plawww/home.cljs
+++ src/cljs/plawww/home.cljs
@@ -3,9 +3,23 @@
 (defn home-page []
 
   [:div.home-page
-   [:h3 "This is the home page."]
-   [:div]
-   [:a {:href "/media"} "Explore media"]])
+   [:h3 "MENIU PLANETAR"]
+   [:div
+    [:a {:href "/media"} "Exploreaza"]]
+   [:div
+    [:a {:href "/media/tag/music"} "Muzica"]]
+   [:div
+    [:a {:href "/media/tag/radio"} "Emisiuni Radio"]]
+   [:div
+    [:a {:href "/media/tag/tv"} "Emisiuni TV"]]
+   [:div
+    [:a {:href "/media/tag/animatie"} "Animatie"]]
+   [:div
+    [:a {:href "/media/tag/scurtmetraj"} "Scurtmetraje"]]
+   [:div
+    [:a {:href "/media/tag/text"} "Carti si texte"]]
+   [:div
+    [:a {:href "/about"} "Despre proiect"]]])
 
 
 

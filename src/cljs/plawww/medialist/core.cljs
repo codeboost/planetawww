084ca6fe44bb65d823d59d88e2ee1cd640555c6c
Florin Braghis <florin@libertv.ro>
2018-06-29 11:54:38 +0200

    Pass state as argument rather than access globally

--- src/cljs/plawww/medialist/core.cljs
+++ src/cljs/plawww/medialist/core.cljs
@@ -21,7 +21,8 @@
                           :expanded-letters #{}
                           :expanded-tags    #{}
                           :show-all?        false
-                          :detail-items?    false}))
+                          :detail-items?    false
+                          :selected-id ""}))
 
 (defn set-opts [opts]
   (js/console.log "media-page/set-opts: " opts)
@@ -30,17 +31,16 @@
 (defn- detail-title [{:keys [title duration]}]
   (str title " - " (utils/format-duration duration)))
 
-(defn item->li [{:keys [title id description] :as item}]
+(defn item->li [{:keys [title id description] :as item} {:keys [detail-items? selected-id]}]
   "Menu item to hiccup."
-  (let [detail?   (:detail-items? @*state*)
-        selected? (= id (:selected-id @*state*))
-        title     (if detail? (detail-title item) title)]
+  (let [selected? (= id selected-id)
+        title     (if detail-items? (detail-title item) title)]
     ^{:key id}
     [:li.media-item
      [:a {:href  (str "/media/" id)
           :class (when selected? :selected)}
       title]
-     (when detail?
+     (when detail-items?
        [:div.description [:i (when description
                                (-> description
                                    (str/replace "<p>" "")   ;Rudimentary and temporary
@@ -53,10 +53,10 @@
 (defn toggle-expanded-letter [letter]
   (swap! *state* update-in [:expanded-letters] utils/toggle-item letter))
 
-(defn menu->hiccup [{:keys [title items]} expanded?]
+(defn menu->hiccup [{:keys [title items]} {:keys [expanded? detail-items?]}]
   "Renders a menu and its items.
   A 'menu' in this context is a div which displays a title and optionally a `ul` containing  child items."
-  (let [disp-title (if (:detail-items? @*state*)
+  (let [disp-title (if detail-items?
                        (str title " - " (count items))
                        title)]
     [:div.menu
@@ -73,10 +73,10 @@
 
 (defn tag-component
   ""
-  [{:keys [title items]} expanded?]
+  [{:keys [title items]} opts]
   (let [menu {:title (str/upper-case title)
-              :items (map item->li items)}]
-    [menu->hiccup menu expanded?]))
+              :items (map #(item->li % opts) items)}]
+    [menu->hiccup menu opts]))
 
 (defn- expand? [title selected-tags]
   (let [clean-str      (comp str/trim str/lower-case)
@@ -88,9 +88,9 @@
 
 (defn render-tag-components
   "Given a list of items grouped by tags, render the tag titles and, if expanded, the items."
-  [tagged expanded-tags show-all?]
+  [tagged {:keys [expanded-tags show-all?] :as opts}]
   (map (fn [{:keys [title] :as item}]
-         [tag-component item (or show-all? (expand? title expanded-tags))])
+         [tag-component item (assoc opts :expanded? (or show-all? (expand? title expanded-tags)))])
        tagged))
 
 (defonce by-tags* (memoize media-db/by-tags))
@@ -99,19 +99,21 @@
   "Collection of Tag components. Each Tag component has a bunch of items, which are hidden by default.
   Only items in `expanded-tags` Tags are visible.
   If `show-all?` then all items are visible."
-  [media-items expanded-tags show-all?]
+  [media-items opts]
   (let [tagged (by-tags* media-items)
-        comps  (render-tag-components tagged expanded-tags show-all?)]
+        comps  (render-tag-components tagged opts)]
     (into [:div.media-items.horiz-container] comps)))
 
-
+;Todo: Search needs its own state atom
 (defn render-search-results [items s expanded-tags no-results-fn]
   (let [items (media-db/search-in-items items s)
         tagged-items (media-db/filter-tagged (by-tags* items) items s)]
     [:div.search-results
      (when (seq tagged-items)
        [:div.tags
-        (into [:div] (render-tag-components tagged-items expanded-tags false))
+        (into [:div] (render-tag-components tagged-items {:expanded-tags expanded-tags
+                                                          :show-all? false
+                                                          :detail-items? false}))
         [:div "---"]])
      [:ul.items
       (if (seq items)
@@ -119,7 +121,6 @@
         [:li.no-results no-results-fn])]]))
 
 (defn- group-by-first-letter [items]
-  (println "group-by-first-letter called.")
   (group-by #(utils/extract-first-letter (:title %)) items))
 
 (def group-by-first-letter* (memoize group-by-first-letter))
@@ -140,14 +141,14 @@
   `items` - items to display.
   `expanded` - a set of letters. The items in these letter are displayed, otherwise only the letter is visible.
   `show-all` - all items are displayed."
-  [items expanded show-all?]
+  [items {:keys [expanded-letters show-all?]}]
   ;TODO: scroll to *letter
   (let [items-by-first-letter (group-by-first-letter* items)
         first-letters         (sort (keys items-by-first-letter))]
     (into [:ul.all-letters]
       (map
        (fn [letter]
-         (let [show-items? (or show-all? (expanded letter))]
+         (let [show-items? (or show-all? (expanded-letters letter))]
            ^{:key (str "L_" letter)}
            [:li
             [letter-component letter]
@@ -157,10 +158,10 @@
        first-letters))))
 
 (defn media-items-component [items state]
-  (let [{:keys [group-by expanded-tags expanded-letters show-all?]} state]
+  (let [{:keys [group-by]} state]
     (case group-by
-      :tag [items-by-tag items expanded-tags show-all?]
-      :plain [items-by-alphabet items expanded-letters show-all?])))
+      :tag [items-by-tag items state]
+      :plain [items-by-alphabet items state])))
 
 (defn media-page [media-items]
   (fn []
@@ -175,6 +176,11 @@
 (comment
  (def media-items (session/get :media-items))
  (sort (keys (group-by-first-letter* media-items)))
+
+ (media-items-component media-items @*state*)
+
+ (-> media-items
+     media-page)
 
  (-> media-items
      (search-in-tags "musi")

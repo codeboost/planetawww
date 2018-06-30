599522b7e7918b0dc4a8fb33a195229742d979b2
Florin Braghis <florin@libertv.ro>
2018-06-30 15:56:28 +0200

    Use one button for tag/plain group by. Create cycle-button

--- src/cljs/plawww/medialist/toolbar.cljs
+++ src/cljs/plawww/medialist/toolbar.cljs
@@ -8,29 +8,45 @@
     [toggle-button {:on-click #(swap! *state update-in [:show-all?] not)
                     :class-name (when (:show-all? @*state) :on)} "TOT"]))
 
-(defn- button-tags [selected?]
-  [toggle-button
-   {:class-name (if selected? :on "")
-    :href "/media/tag"} "TAG"])
+(defn cycle-button
+  "A button component which cycles through several possible texts/states, defined in `items`.
+  When user clicks the button, the button transitions to the next state, displaying a new text.
+  The state value is recorded in the `state` atom, in the keypath `ks`.
+  `state` is an atom which contains current state (in the keypath defined by `ks`)
+  `items` is a map with the keys being used as values in the atom state and values are the button titles.
+  Example usage:
 
-(defn- button-letters [selected?]
-  [toggle-button
-   {:class-name (if selected? :on "")
-    :href "/media/letter"} "LIT"])
+      [cycle-button
+        (into (sorted-map) {:tag \"TAG\" :plain \"LIT\"})
+        [:group-by]]
+  "
+  [items *state ks]
+  (let [states (vec (keys items))
+        titles (vec (vals items))
+        ;Returns the next index based on current state 'cur'.
+        next-index (fn [i]
+                     (let [i (if (< i 0) 0 i)]
+                          (mod (inc i) (count states))))]
+    (fn [_ *state _]
+      (let [cur (get-in @*state ks)
+            i (.indexOf states cur)
+            title (nth titles (next-index i))]
+        [toggle-button
+         {:on-click (fn []
+                      (swap! *state update-in ks #(nth states (next-index (.indexOf states cur)))))}
+         title]))))
 
-(def av-states ["AV" "A." ".V"])
+(defn button-group-by [*state]
+  [cycle-button
+   (into (sorted-map) {:tag "TAG" :plain "LIT"})
+   *state
+   [:group-by]])
 
-(defn button-av-handler [*state]
-  (swap! *state update-in [:av] #(mod (inc %) (count av-states))))
-
-(defn- button-av [*state]
-  (let [i (int (:av @*state))
-        title (nth av-states i)
-        handler (fn [_] (swap! *state update-in [:av] #(mod (inc %) (count av-states))))]
-    [toggle-button
-     {:on-click handler
-      :class-name (when (pos? i) :on)}
-     title]))
+(defn button-av [*state]
+  [cycle-button
+   {:av "AV" :a "A." :v ".V"}
+   *state
+   [:av]])
 
 (defn- button-item-details [*state]
   (fn []
@@ -47,5 +63,4 @@
          [button-all *state]
          [button-av *state]
          [button-item-details *state]
-         [button-tags (= :tag (:group-by @*state))]
-         [button-letters (= :plain (:group-by @*state))]]))))
+         [button-group-by *state]]))))

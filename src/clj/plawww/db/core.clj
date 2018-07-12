9ddbb5f84e6e24f6a633652acbe82845bd2a2260
Florin Braghis <florin@libertv.ro>
2018-07-12 10:57:54 +0200

    Use hsql to build the query

--- src/clj/plawww/db/core.clj
+++ src/clj/plawww/db/core.clj
@@ -10,19 +10,25 @@
                 "mysql://root:%s@mysql:3306/mediacore?charset=utf8&use_unicode=0"
                 (env :planeta-mysql-pwd)))
 
-(defn get-media-hsql-unfinished []
+(defn get-media-hsql []
   (let [mfq (hsql/build :select [:media_id
                                  :unique_id
-                                 :unique_id :filename
+                                 [:unique_id :filename]
                                  :type
                                  :size
                                  :width
-                                 :height]
-                        :from [:media_files :mf])
-        mtq (hsql/build :select []
-                        :from [:media_tags :mt])
+                                 :height
+                                 :storage_id]
+                        :from [:media_files])
+        mtq (hsql/build
+             {:select [:media_id
+                       [(hsql/raw "GROUP_CONCAT(t.name SEPARATOR ', ')") :tags]]
+              :from [[:media_tags :mt]]
+              :join [[:tags :t]
+                     [:= :t.id :mt.tag_id]]
+              :group-by [:media_id]})
         sql (hsql/build
-              :select [:id
+             {:select [:id
                        :mf.type
                        :slug
                        :publish_on
@@ -32,7 +38,7 @@
                        :description_plain
                        :duration
                        :views
-                       :media_id
+                       :mt.media_id
                        :tags
                        :unique_id
                        :filename
@@ -40,20 +46,24 @@
                        :width
                        :height]
               :from :media
-              ; media_id, unique_id, unique_id as filename, type, size,  width, height FROM media_files
-              :inner-join [])]
+              :join [[mfq :mf] [:= :mf.media_id :id]]
+              :left-join [[mtq :mt] [:= :mt.media_id :id]]})]
 
     (j/query mysql-uri (hsql/format sql))))
 
 (def get-media-sql-str
-  "SELECT id, mf.type, slug, publish_on, title, subtitle, description, description_plain, duration, views, tags, filename, size, width, height, unique_id FROM media
-  INNER JOIN (SELECT media_id, unique_id, unique_id as filename, type, size,  width, height FROM media_files) AS mf ON mf.media_id = id
-  LEFT JOIN (SELECT media_id, GROUP_CONCAT(tags.name SEPARATOR ', ') AS tags FROM media_tags INNER JOIN tags ON tag_id = tags.id GROUP BY media_id) AS mt ON mt.media_id = id;")
+  "SELECT id, mf.type, slug, publish_on, title, subtitle, description, description_plain, duration, views, tags, filename, size, width, height, unique_id
+  FROM media
+  INNER JOIN
+    (SELECT media_id, unique_id, unique_id as filename, type, size,  width, height FROM media_files) AS mf ON mf.media_id = id
+  LEFT JOIN
+    (SELECT media_id, GROUP_CONCAT(tags.name SEPARATOR ', ') AS tags FROM media_tags
+     INNER JOIN tags ON tag_id = tags.id GROUP BY media_id) AS mt ON mt.media_id = id;")
 
 
 (defn get-media []
-  (j/query mysql-uri get-media-sql-str))
+  (get-media-hsql))
 
 
 (comment
- (get-media))
+ (get-media-hsql-unfinished))

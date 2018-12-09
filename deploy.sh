b8f481e70426bdad7f16bfcf5246ea7108827b49
Florin Braghis <florin@libertv.ro>
2018-12-09 23:32:45 +0100

    Syntax error in script?

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #Creates uberjar, then builds the docker container and uploads it to docker hub.
 
 if git diff-index --quiet HEAD --; then
-    echo "Repository contains no unstanged changes, that's good."
+    echo "Repository contains no unstaged changes, that's good."
 else
 	echo "Repository has changes. Please commit changes before deploying."
 	exit 1
@@ -9,12 +9,13 @@ fi
 
 echo "Creating uberjar..."
 lein uberjar
+
 if [ $? -eq 0 ]; then
     echo "Rebuilding docker container..."
 	docker build -t florinbraghis/planeta-crt .
 	if [ $? -eq 0 ]; then
 		echo "Pushing..."
-		if [[ $* == *-vm ]]
+		if [[ $* == *-vm ]];
 		then
 			echo "Incrementing minor version"
 			semver inc minor
@@ -23,8 +24,7 @@ if [ $? -eq 0 ]; then
 			git tag -a `semver tag`
 		fi
 
-		if [[ $* == *-vp ]]
-		then
+		if [[ $* == *-vp ]]; then
 			echo "Incrementing patch version"
 			semver inc patch
 			git add '.semver'

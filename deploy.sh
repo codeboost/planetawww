e00084f289f3d6e8c2065033e74c4c6fc858c685
Florin Braghis <florin@libertv.ro>
2018-12-10 13:35:00 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,36 @@
+#!/bin/bash
+
 #Creates uberjar, then builds the docker container and uploads it to docker hub.
 
-if git diff-index --quiet HEAD --; then
-    echo "Repository contains no unstaged changes, that's good."
-else
-	echo "Repository has changes. Please commit changes before deploying."
-	exit 1
-fi
+set -e
+
+function bail-if-changes-exist {
+	if git diff-index --quiet HEAD --; then
+	    echo "Repository contains no unstaged changes, that's good."
+	else
+		echo "Repository has changes. Please commit changes before deploying."
+		exit 1
+	fi
+}
+
+function bail-if-not-on-branch {
+	EXPECTED_BRANCH=$1
+	BRANCH=$(git rev-parse --abbrev-ref HEAD)
+	echo "On branch $BRANCH"
+	if [[ "$BRANCH" != "$EXPECTED_BRANCH" ]]; then
+	  echo "Must be on '$EXPECTED_BRANCH' git branch to deploy."
+	  exit 1
+	fi	
+}
+
+bail-if-not-on-branch master
+bail-if-changes-exist
 
 echo "Creating uberjar..."
 lein uberjar
 
 if [ $? -eq 0 ]; then
-    echo "Rebuilding docker container..."
-	docker build -t florinbraghis/planeta-crt .
 	if [ $? -eq 0 ]; then
-		echo "Pushing..."
 		if [[ $* == *-vm ]];
 		then
 			echo "Incrementing minor version"
@@ -31,9 +47,12 @@ if [ $? -eq 0 ]; then
 			git commit -m 'Updated patch version.'
 			git tag -a `semver tag`
 		fi
+	
+	    echo "Rebuilding docker container..."
+		docker build -t florinbraghis/planeta-crt -t florinbraghis/planeta-crt:`semver tag` .
 
 		echo "Pushing `semver tag`"			
-		docker push florinbraghis/planeta-crt
+		docker push florinbraghis/planeta-crt:`semver tag`
 	else
 		echo "Error: docker build failed."
 	fi

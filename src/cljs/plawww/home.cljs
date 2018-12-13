71c09d318affd3a7a09828d9bf6d44966df11dea
Florin Braghis <florin@libertv.ro>
2018-12-13 01:04:16 +0100

    Use CSS Variables for colors, apply-style implementation

--- src/cljs/plawww/home.cljs
+++ src/cljs/plawww/home.cljs
@@ -6,7 +6,10 @@
 ;   You must not remove this notice, or any other, from this software.
 
 (ns plawww.home
-  (:require [plawww.paths :refer [explorer-path]]))
+  (:require
+   [plawww.paths :refer [explorer-path]]
+   [reagent.core :as r])
+  (:require-macros [reagent.interop :refer [$ $!]]))
 
 (def titles
   ["MENIU"
@@ -50,6 +53,48 @@
     [menu-item "tag/animatie" "Animatie"]
     [menu-item "tag/scurtmetraj" "Scurtmetraje"]
     [menu-item "/text" "Carti si texte"]
-    [menu-item "/about" "Despre proiect"]]])
+    [menu-item "/about" "Despre proiect"]
+    [:div {:on-click (fn [])} "Settings"]]])
+
+(def styles
+  [{:name "Planeta Default"
+    :color "#eee"
+    :border "#eee"
+    :background "#383838"
+    :highlight "#fff"
+    :dark-border "#1e1e1e"}
+   {:name "Violet"
+    :color "#8580BB"
+    :border "#8580BB"
+    :background "#27226D"
+    :highlight "#fff"
+    :dark-border "#1e1e1e"}
+   {:name "CRT"
+    :color "#14fdce"
+    :border "#14fdce"
+    :background "#031e11"
+    :highlight "#fff"
+    :dark-border "#031e11"}])
 
 
+
+(defn apply-style [{:keys [color background highlight border dark-border]
+                    :or {highlight color
+                         border color
+                         dark-border background}}]
+  (let [document-element ($ js/document :documentElement)
+        style ($ document-element :style)]
+    ($ style setProperty "--console-bg" background)
+    ($ style setProperty "--console-color" color)
+    ($ style setProperty "--border-color" border)
+    ($ style setProperty "--highlight-color" highlight)
+    ($ style setProperty "--dark-border-color" dark-border)))
+
+
+
+
+(comment
+ (apply-style (get styles 2))
+ (let [document-element ($ js/document :documentElement)
+       style ($ document-element :style)]
+   ($ style setProperty "--console-bg" "#222")))

b578b269e6694bb2e1a5fe19400d2682ecc3d2bc
Florin Braghis <florin@libertv.ro>
2020-05-09 17:52:58 +0200

    Remove devcards

--- project.clj
+++ project.clj
@@ -6,22 +6,22 @@
 
   :dependencies [[binaryage/devtools "1.0.0"]
                  [binaryage/dirac "1.5.11"]
-                 [com.cemerick/url "0.1.1"]
                  [clj-http "3.10.1"]
-                 [cljsjs/typedjs "1.1.1-0"] ;jquery plugin that types
-                 [cljsjs/react-player "1.11.0-0"]
-                 [compojure "1.6.1"]
                  [cljsjs/google-analytics "2017.09.21-0"]
+                 [cljsjs/react-player "1.11.0-0"]
+                 [cljsjs/typedjs "1.1.1-0"] ;jquery plugin that types
+                 [com.cemerick/url "0.1.1"]
+                 [compojure "1.6.1"]
                  [hiccup "1.0.5"]
                  [honeysql "0.9.10"]
                  [mysql/mysql-connector-java "8.0.20"]
-                 [org.clojure/java.jdbc "0.7.11"]
                  [org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.339" :scope "provided"]
                  [org.clojure/core.async "1.1.587"]
+                 [org.clojure/java.jdbc "0.7.11"]
                  [reagent "0.8.1"]
                  [reagent-utils "0.3.1"]
-                 [ring "1.8.0"]
+                 [ring "1.8.1"]
                  [ring-server "0.5.0"]
                  [ring/ring-defaults "0.3.2"]
                  [secretary "1.2.3"]
@@ -81,22 +81,7 @@
               :optimizations :none
               :pretty-print  true
               :foreign-libs [{:file "resources/public/lib/screenfull.js"
-                              :provides ["screenfull"]}]}}
-
-
-            :devcards
-            {:source-paths ["src/cljs" "src/cljc" "env/dev/cljs"]
-             :figwheel {:devcards true}
-             :compiler {:main "plawww.cards"
-                        :asset-path "js/devcards_out"
-                        :output-to "target/cljsbuild/public/js/app_devcards.js"
-                        :output-dir "target/cljsbuild/public/js/devcards_out"
-                        :source-map-timestamp true
-                        :optimizations :none
-                        :pretty-print true}}}}
-
-
-
+                              :provides ["screenfull"]}]}}}}
 
   :figwheel
   {:http-server-root "public"
@@ -115,12 +100,11 @@
                                   :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
 
                    :dependencies [[ring/ring-mock "0.4.0"]
-                                  [ring/ring-devel "1.8.0"]
+                                  [ring/ring-devel "1.8.1"]
                                   [prone "2020-01-17"]
                                   [figwheel-sidecar "0.5.16"]
                                   [org.clojure/tools.nrepl "0.2.13"]
                                   [com.cemerick/piggieback "0.2.2"]
-                                  [devcards "0.2.6"]
                                   [pjstadig/humane-test-output "0.10.0"]]
 
 
